3fb184248ead42811297278d7a68949fe7a4b755
Andreas Johansson <glanshammar70@gmail.com>
2024-10-01 15:08:48 +0200

    Added global usings.

--- Garage V1/GlobalUsings.cs
+++ Garage V1/GlobalUsings.cs
@@ -4,3 +4,5 @@ global using System.Collections.Generic;
 global using System.IO;
 global using System.Linq;
 global using Garage_V1.Interfaces;
+global using Garage_V1.Vehicles;
+global using Garage_V1.Garage;
