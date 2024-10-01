d4a13ba9818ba3eb68d55d80bf8ad0497e8eedeb
Andreas Johansson <glanshammar70@gmail.com>
2024-10-01 19:27:55 +0200

    Added IHandler functionality

--- Garage V1/Interfaces/IHandler.cs
+++ Garage V1/Interfaces/IHandler.cs
@@ -2,5 +2,11 @@
 
 public interface IHandler
 {
-    
+    int Capacity { get; }
+    bool ParkVehicle(Vehicle vehicle);
+    bool RetrieveVehicle(string registrationNumber);
+    List<Vehicle> GetAllParkedVehicles();
+    Dictionary<Type, int> GetVehicleTypeCounts();
+    List<Vehicle> SearchVehicles(Func<Vehicle, bool> predicate);
+    int GetAvailableSpaces();
 }
