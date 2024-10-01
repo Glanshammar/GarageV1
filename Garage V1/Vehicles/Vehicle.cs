a3bd0bc2e88daae6537a0f6d3fe4ced9b50f4bdd
Andreas Johansson <glanshammar70@gmail.com>
2024-10-01 15:10:16 +0200

    Added abstract class for Vehicle.

---
+++ Garage V1/Vehicles/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿namespace Garage_V1.Vehicles;
+
+public abstract class Vehicle : IVehicle
+{
+    public string RegistrationNumber { get; set; }
+    public string Color { get; set; }
+    public int NumberOfWheels { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+
+    protected Vehicle(string color, int numberOfWheels, string registrationNumber = null!)
+    {
+        RegistrationNumber = registrationNumber ?? RandomRegistrationNumber();
+        Color = color;
+        NumberOfWheels = numberOfWheels;
+    }
+
+    public override string ToString()
+    {
+        return $"{GetType().Name}: Reg.Nr: {RegistrationNumber}, Color: {Color}, Wheels: {NumberOfWheels}";
+    }
+
+    private static string RandomRegistrationNumber()
+    {
+        Random random = new Random();
+    
+        // Generate 3 random letters
+        string randomLetters = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3)
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+
+        // Generate 3 random single-digit numbers
+        string randomNumbers = new string(Enumerable.Repeat("0123456789", 3)
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+
+        // Combine letters and numbers
+        return randomLetters + randomNumbers;
+    }
+}
